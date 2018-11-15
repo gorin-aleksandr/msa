5163f554f9c7dfc65d8086a4c74b9e6300fb9ea1
Andrey <andrey.krit@gmail.com>
2018-11-15 23:49:17 +0200

    Added heart-rate details vc

---
+++ MSA/Exercises/VC/HeartBeatDevice/HeartBeatDetailsPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  HeartBeatDetailsPresenter.swift
+//  MSA
+//
+//  Created by Andrey Krit on 11/14/18.
+//  Copyright © 2018 Pavlo Kharambura. All rights reserved.
+//
+
+import Foundation
+
+
+class HeartBeatDetailsPresenter {
+    
+}
