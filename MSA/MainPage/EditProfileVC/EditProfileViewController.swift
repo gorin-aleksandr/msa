571673727ea02b5903d8ca87fb722a1222178eb3
Andrey <andrey.krit@gmail.com>
2019-03-16 17:32:41 +0200

    Added alert on logout

--- MSA/MainPage/EditProfileVC/EditProfileViewController.swift
+++ MSA/MainPage/EditProfileVC/EditProfileViewController.swift
@@ -349,20 +349,24 @@ class EditProfileViewController: UIViewController, UIImagePickerControllerDelega
     @IBAction func rules(_ sender: Any) {
     }
     @IBAction func logout(_ sender: Any) {
+        AlertDialog.showConfirmationAlert("Выход из приложения", message: "Вы действительно хотите выйти из приложения?", viewController: self, confirmAction: logout)
+    }
+    
+    func logout() {
         presenter.deleteUserBlock(context: context) { (loggedOut) in
-        self.presenter.clearRealm()
-        if loggedOut {
+            self.presenter.clearRealm()
+            if loggedOut {
                 let storyBoard = UIStoryboard(name: "Main", bundle:nil)
                 let nextViewController = storyBoard.instantiateViewController(withIdentifier: "MainNVC") as! UINavigationController
                 self.navigationController?.show(nextViewController, sender: self)
                 AuthModule.currUser.id = nil
                 
             } else {
-                AlertDialog.showAlert("Ошибка", message: "Ошибка логаута", viewController: self)
+                AlertDialog.showAlert("Ошибка", message: "Ошибка при выходе из приложения", viewController: self)
             }
         }
-        
     }
+    
     @IBAction func hideTrainerView(_ sender: Any) {
 
     }
