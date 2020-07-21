0e0bca9a0e373b54235246f7a33ca4fca6b1f899
Nik <nikshebeda@gmail.com>
2020-07-21 19:59:51 +0300

    Fixed swipe to delete itterations

--- MSA/Trannings/VC/IterationsViewController.swift
+++ MSA/Trannings/VC/IterationsViewController.swift
@@ -405,7 +405,9 @@ extension IterationsViewController: SwipeTableViewCellDelegate {
             guard let object = self.manager.getCurrentExercise()?.iterations[indexPath.row] else {return}
             self.manager.realm.deleteObject(object)
             self.manager.editTraining(wiht: self.manager.getCurrentTraining()?.id ?? -1, success: {})
-            UIView.transition(with: self.tableView, duration: 0.35, options: .transitionCrossDissolve, animations: { self.tableView.reloadData() })
+            UIView.transition(with: self.tableView, duration: 0.35, options: .transitionCrossDissolve, animations: {
+           //   self.tableView.reloadData()
+            })
         }
         deleteAction.image = UIImage(named: "cancel-24px")
         deleteAction.title = nil
